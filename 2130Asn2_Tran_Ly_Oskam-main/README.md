bbcd8b3413ed797511e5c7254a14defc18466ecd
Justin Oskam <151467881+J-Oskam@users.noreply.github.com>
2024-04-14 22:26:44 -0400

    Delete 2130Asn2_Tran_Ly_Oskam-main directory

--- 2130Asn2_Tran_Ly_Oskam-main/README.md
+++
@@ -1,2 +0,0 @@
-UML Link: https://drive.google.com/file/d/1qQnJXCepoEjf_p0wTfiUj3Y6UCEPUMPQ/view?usp=sharing
-
--- 2130Asn2_Tran_Ly_Oskam-main/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.hellofx {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.flightSystem to javafx.fxml;
-    exports com.flightSystem;
-}
